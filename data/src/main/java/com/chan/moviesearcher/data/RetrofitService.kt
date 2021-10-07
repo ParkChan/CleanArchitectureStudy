0bef254e0adffe4e67dc0ca5775a8068a7a00048
ParkChan <hydrarkk@naver.com>
2021-10-07 23:16:10 +0900

    [PR/수정] P2. 서비스를 생성하는 부분은 다른 클래스에서 구현하면 좋을 것 같아요

--- data/src/main/java/com/chan/moviesearcher/data/RetrofitService.kt
+++ data/src/main/java/com/chan/moviesearcher/data/RetrofitService.kt
@@ -1,48 +1,14 @@
 package com.chan.moviesearcher.data
 
+import com.chan.moviesearcher.data.NetworkModule.httpLoggingInterceptor
+import com.chan.moviesearcher.data.NetworkModule.kotlinJsonAdapterFactory
+import com.chan.moviesearcher.data.NetworkModule.moshi
+import com.chan.moviesearcher.data.NetworkModule.moshiConverter
+import com.chan.moviesearcher.data.NetworkModule.okHttpClient
+import com.chan.moviesearcher.data.NetworkModule.retrofitBuild
 import com.chan.moviesearcher.data.source.MovieApi
-import com.squareup.moshi.Moshi
-import com.squareup.moshi.kotlin.reflect.KotlinJsonAdapterFactory
-import okhttp3.OkHttpClient
-import okhttp3.logging.HttpLoggingInterceptor
-import retrofit2.Retrofit
-import retrofit2.converter.moshi.MoshiConverterFactory
 
 internal object RetrofitService {
-
-    private const val BASE_URL = "https://openapi.naver.com/"
-
-    private fun httpLoggingInterceptor() = HttpLoggingInterceptor()
-        .apply {
-            level = HttpLoggingInterceptor.Level.BODY
-        }
-
-    private fun kotlinJsonAdapterFactory() = KotlinJsonAdapterFactory()
-
-    private fun moshi(jsonAdapterFactory: KotlinJsonAdapterFactory): Moshi =
-        Moshi.Builder()
-            .add(jsonAdapterFactory)
-            .build()
-
-    private fun moshiConverter(moshi: Moshi): MoshiConverterFactory =
-        MoshiConverterFactory.create(moshi)
-
-    private fun okHttpClient(
-        loggingInterceptor: HttpLoggingInterceptor,
-    ): OkHttpClient =
-        OkHttpClient.Builder()
-            .addInterceptor(loggingInterceptor)
-            .build()
-
-    private fun retrofitBuild(
-        converterFactory: MoshiConverterFactory,
-        client: OkHttpClient
-    ): Retrofit =
-        Retrofit.Builder()
-            .baseUrl(BASE_URL)
-            .addConverterFactory(converterFactory)
-            .client(client)
-            .build()
 
     private fun movieService(): MovieApi =
         retrofitBuild(
@@ -54,7 +20,7 @@ internal object RetrofitService {
             okHttpClient(httpLoggingInterceptor())
         ).create(MovieApi::class.java)
 
-    val movieApi: MovieApi by lazy {
+    internal val movieApi: MovieApi by lazy {
         movieService()
     }
 }
