b7cbdbb44894478022b7f908352982d2b991c24b
ParkChan <hydrarkk@naver.com>
2021-10-07 23:17:59 +0900

    [PR/수정] P3. 레트로핏 서비스 생성 시 suspend 함수로 만들면 내부적으로 io 디스패처에서 구동됩니다.

--- data/src/main/java/com/chan/moviesearcher/data/source/MovieDataSource.kt
+++ data/src/main/java/com/chan/moviesearcher/data/source/MovieDataSource.kt
@@ -1,13 +1,8 @@
 package com.chan.moviesearcher.data.source
 
 import com.chan.moviesearcher.data.entity.MovieResponse
-import kotlinx.coroutines.CoroutineDispatcher
-import kotlinx.coroutines.Dispatchers
 
 internal interface MovieDataSource {
-
-    val ioDispatcher: CoroutineDispatcher
-        get() = Dispatchers.IO
 
     suspend fun fetchMovies(page:Int, query: String) : MovieResponse
 }
