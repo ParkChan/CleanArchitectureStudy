aec0b5e2c7b1dc3c4112708487a5d4cbeea93276
ParkChan <hydrarkk@naver.com>
2021-10-08 23:52:16 +0900

    [PR/수정] P2. 불필요한 어노테이션 같아요

--- data/src/main/java/com/chan/moviesearcher/data/DateJsonAdapter.kt
+++ data/src/main/java/com/chan/moviesearcher/data/DateJsonAdapter.kt
@@ -7,22 +7,21 @@ import java.io.IOException
 import java.text.SimpleDateFormat
 import java.util.*
 
-class DateJsonAdapter : JsonAdapter<Date?>() {
+class DateJsonAdapter : JsonAdapter<Date>() {
 
     private val dateFormat = SimpleDateFormat(NAVER_DATE_FORMAT, Locale.US)
 
     @Synchronized
     @Throws(IOException::class)
-    override fun fromJson(reader: JsonReader): Date? {
+    override fun fromJson(reader: JsonReader): Date {
         return if (reader.peek() == JsonReader.Token.NULL) {
-            reader.nextNull<Any>() as Date?
+            reader.nextNull<Any>() as Date
         } else {
             val string = reader.nextString()
             return dateFormat.parse(string)
         }
     }
 
-    @Suppress("SimpleDateFormat")
     @Synchronized
     @Throws(IOException::class)
     override fun toJson(writer: JsonWriter, value: Date?) {
