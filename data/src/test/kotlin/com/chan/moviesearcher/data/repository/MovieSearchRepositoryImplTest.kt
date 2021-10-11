7689b8ee25216f3752cc0111f05f972e7a7156d4
ParkChan <hydrarkk@naver.com>
2021-10-11 23:22:34 +0900

    [PR/수정] P1. Repository와 UseCase는 다른 개념입니다.         Repository 인터페이스를 implementation 해주세요.         프로그래밍 요구사항의 첫번째 항목을 참고해주세요.

--- data/src/test/kotlin/com/chan/moviesearcher/data/repository/MovieSearchRepositoryImplTest.kt
+++ data/src/test/kotlin/com/chan/moviesearcher/data/repository/MovieSearchRepositoryImplTest.kt
@@ -2,6 +2,7 @@ package com.chan.moviesearcher.data.repository
 
 import com.chan.moviesearcher.data.entity.MovieResponse
 import com.chan.moviesearcher.data.source.MovieDataSourceImpl
+import com.chan.moviesearcher.domain.dto.MovieDto
 import io.mockk.coEvery
 import io.mockk.mockk
 import kotlinx.coroutines.runBlocking
@@ -9,27 +10,27 @@ import org.junit.jupiter.api.Assertions.assertEquals
 import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.Test
 
-class MovieSearchRepositoryTest {
+class MovieSearchRepositoryImplTest {
 
     private val movieDataSourceImpl: MovieDataSourceImpl = mockk(relaxed = true)
-    private lateinit var repository: MovieSearchRepository
+    private lateinit var repositoryImpl: MovieSearchRepositoryImpl
 
     @BeforeEach
     fun setup() {
-        repository = MovieSearchRepository(movieDataSourceImpl)
+        repositoryImpl = MovieSearchRepositoryImpl(movieDataSourceImpl)
     }
 
     @Test
-    internal fun `영화 리스트 가져오기 기능 테스트`() = runBlocking {
-        val mockResponse: MovieResponse = mockk()
+    fun `영화 리스트 가져오기 기능 테스트`() = runBlocking {
+        val mockResponse: MovieResponse = mockk(relaxed = true)
 
         coEvery {
             movieDataSourceImpl.fetchMovies(start = 1, query = "a")
         } returns mockResponse
 
-        val result = repository
+        val result: MovieDto = repositoryImpl
             .fetchMovies(start = 1, query = "a")
 
-        assertEquals(mockResponse.display, result.display)
+        assertEquals(MovieResponse.mapToDto(mockResponse), result)
     }
 }
