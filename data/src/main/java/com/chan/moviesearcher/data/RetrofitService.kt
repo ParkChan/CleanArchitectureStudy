4cca713afa91e296b3bffe0ccc35e7b6818d4d85
ParkChan <hydrarkk@naver.com>
2021-10-13 17:12:37 +0900

    [수정] Data Module : 통신 모듈 DI로 변경하면서 Object -> class 로 변경

--- data/src/main/java/com/chan/moviesearcher/data/RetrofitService.kt
+++ data/src/main/java/com/chan/moviesearcher/data/RetrofitService.kt
@@ -10,7 +10,7 @@ import javax.inject.Singleton
 
 @Module
 @InstallIn(SingletonComponent::class)
-internal object RetrofitService {
+internal class RetrofitService {
 
     @Provides
     @Singleton
