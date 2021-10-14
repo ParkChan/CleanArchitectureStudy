a7290e664366c05f472c1946725b7b7a44681418
ParkChan <hydrarkk@naver.com>
2021-10-14 14:29:36 +0900

    [PR/수정] MovieSearchViewModelTest 수정 P2. 미사용 테스트 코드는 제거해주세요 P2. ExperimentalCoroutinesApi 어노테이션은 없어도 될 것 같아요 P1. 테스트 함수 이름을 보다 직관적으로 작성해보면 어떨까요? 예를 들면 영화 리스트를 불러옵니다 와 같은 이름으로 작성할 수도 있을 것 같습니다. P2. JUnit5에는 테스트 함수명이 한글로 가능하기에, DisplayName은 사용하지 않으셔도 됩니다.

--- app/src/test/kotlin/com/chan/moviesearcher/viewmodel/MovieSearchViewModelTest.kt
+++ app/src/test/kotlin/com/chan/moviesearcher/viewmodel/MovieSearchViewModelTest.kt
@@ -3,37 +3,28 @@ package com.chan.moviesearcher.viewmodel
 import com.chan.moviesearcher.domain.dto.MovieDto
 import com.chan.moviesearcher.domain.usecase.MovieSearchUseCaseImpl
 import com.chan.moviesearcher.util.InstantTaskExecutorExtension
-import com.chan.moviesearcher.util.MainCoroutineRule
 import com.chan.moviesearcher.util.getOrAwaitValue
 import io.mockk.coEvery
 import io.mockk.mockk
-import kotlinx.coroutines.ExperimentalCoroutinesApi
 import kotlinx.coroutines.runBlocking
 import org.junit.jupiter.api.Assertions.assertEquals
 import org.junit.jupiter.api.BeforeEach
-import org.junit.jupiter.api.DisplayName
 import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.extension.ExtendWith
 
 @ExtendWith(InstantTaskExecutorExtension::class)
 class MovieSearchViewModelTest {
 
-    @ExperimentalCoroutinesApi
-    private lateinit var mainCoroutineRule: MainCoroutineRule
     private val useCase: MovieSearchUseCaseImpl = mockk(relaxed = true)
     private lateinit var viewModel: MovieSearchViewModel
 
-    @ExperimentalCoroutinesApi
     @BeforeEach
     fun setUp() {
-        mainCoroutineRule = MainCoroutineRule()
         viewModel = MovieSearchViewModel(useCase)
     }
 
-    @ExperimentalCoroutinesApi
     @Test
-    @DisplayName("라이브 데이터 테스트 입니다")
-    fun `라이브 데이터 테스트`() = runBlocking {
+    fun `영화 리스트를 불러옵니다`() = runBlocking {
 
         val mockRes: MovieDto = mockk(relaxed = true)
 
