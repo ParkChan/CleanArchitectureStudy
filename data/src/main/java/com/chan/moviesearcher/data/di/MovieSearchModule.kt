c6bd2a2e2584169dca3bc9c1e376a926d6a27707
iron883 <hydrarkk@naver.com>
2023-04-27 23:35:40 +0900

    [수정] 파라미터 수정

--- data/src/main/java/com/chan/moviesearcher/data/di/MovieSearchModule.kt
+++ data/src/main/java/com/chan/moviesearcher/data/di/MovieSearchModule.kt
@@ -23,7 +23,7 @@ internal abstract class MovieSearchModule {
     @Binds
     @Singleton
     abstract fun bindsMovieSearchRepository(
-        movieSearchRepositoryImpl: MovieSearchRepositoryImpl
+        movieSearchRepository: MovieSearchRepositoryImpl
     ): MovieSearchRepository
 
 }
